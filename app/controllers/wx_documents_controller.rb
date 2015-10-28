c67fa8e4e2ca7ab2c1f60c359a4e91ec675db7fb
wangning <wangning2046@gmail.com>
2015-10-28 21:17:59 +0800

    微信数据按时间倒排

--- app/controllers/wx_documents_controller.rb
+++ app/controllers/wx_documents_controller.rb
@@ -2,6 +2,6 @@ class WxDocumentsController < ApplicationController
   before_action -> { @navbar = :wx_document }
 
   def index
-    @docs = WxDocument.all.page params[:page]
+    @docs = WxDocument.all.order("date desc").page params[:page]
   end
 end
